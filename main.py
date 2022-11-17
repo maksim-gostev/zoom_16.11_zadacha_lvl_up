8a7607a2e47c381845a19ec39f7dc198d3bc70bb
Maksim Gostev <teranys1986@gmail.com>
2022-11-17 11:22:17 +0300

    моё решение задачи по повышению уровня с тестовыми принтами для понимания количества итераций

--- main.py
+++ main.py
@@ -8,24 +8,16 @@ class Heroec:
 
     def add_experience(self, point):
         self.xp += point
-        count_hp = 1
-        count_lvl = 1
         next_lvl = self.lvl
         while self.xp >= self.lvl_up:
             self.xp -= self.lvl_up
             next_lvl += 1
             self.lvl_up *= 2
-            print(f'итерация lvl {count_lvl}')
-            count_lvl += 1
 
-        for i in range(next_lvl - self.lvl):
-            print(f'итерация hp {count_hp}')
-            count_hp += 1
             self.hp += self.hp / 2
             if self.hp % 10 > 0 :
                 self.hp = (self.hp // 10 + 1) * 10
         self.lvl = next_lvl
-        print(f'test {self.lvl}')
 
     def get_level(self):
         return f'левел {self.lvl}'
